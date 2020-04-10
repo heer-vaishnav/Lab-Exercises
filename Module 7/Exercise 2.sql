8799ccd0e3a44b37cfe5d6e811776c1a323ebd3f
heer-vaishnav <48050788+heer-vaishnav@users.noreply.github.com>
2020-04-10 15:24:21 +0530

    Module 7 Lab-Exercise

--- Module 7/Exercise 2.sql
+++ Module 7/Exercise 2.sql
@@ -8,7 +8,7 @@ GO
 
 CREATE NONCLUSTERED INDEX [IX_FactInternetSales_PromotionKey] ON [dbo].[FactInternetSales]
 (
-	[PromotionKey] ASC
+[PromotionKey] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 CREATE NONCLUSTERED INDEX [IX_FactIneternetSales_ShipDateKey] ON [dbo].[FactInternetSales]
 (
